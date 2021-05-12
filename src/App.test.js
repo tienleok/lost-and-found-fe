0a4925f1a6bbbe4c7295eef55f701118800b2159
TienLeok Ngo <tienleok@gmail.com>
2021-05-12 17:46:10 +0800

    Fix Jest issues

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from '@testing-library/react'
+import App from './App'
 
 test('renders Users link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Users/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  render(<App />)
+  const linkElement = screen.getByText(/Users/i)
+  expect(linkElement).toBeInTheDocument()
+})
