d80e8ae57063d36b6fe06691841157c2b454fb18
TienLeok Ngo <tienleok@gmail.com>
2021-05-12 16:08:29 +0800

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders Home link', () => {
+test('renders Users link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Home/i);
+  const linkElement = screen.getByText(/Users/i);
   expect(linkElement).toBeInTheDocument();
 });
