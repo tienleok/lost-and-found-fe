9dafff8687d1e57f2eb945e0026172f6e24f7254
Ngo Tien Leok <tienleok@gmail.com>
2021-05-23 08:01:11 +0800

    update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   devServer: {
-    disableHostCheck: true
+    compress: true,
+    public: 'lost-and-found-fe-load-balancer-2137131496.ap-southeast-1.elb.amazonaws.com/'
   }
 }
